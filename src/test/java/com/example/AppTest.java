f8bacaf765c32fa9284e4a112310806c7a1c8165
jonki324 <jonki324@gmail.com>
2021-01-10 19:40:33 +0900

    Jackson使い方追加

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,50 +1,34 @@
 package com.example;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * Unit test for simple App.
  */
-@Slf4j
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+  /**
+   * Create the test case.
+   *
+   * @param testName name of the test case
+   */
+  public AppTest(String testName) {
+    super(testName);
+  }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  /**
+   * Test suite.
+   *
+   * @return the suite of tests being tested.
+   */
+  public static Test suite() {
+    return new TestSuite(AppTest.class);
+  }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        App.exec();
-        assertTrue( true );
-        User user = new User();
-        user.setId(Integer.valueOf(1));
-        user.setName("Bob");
-        assertThat(user.getId(), is(1));
-        assertThat(user.getName(), is("Bob"));
-        log.debug("Slf4j Lombok");
-    }
+  /**
+   * Rigourous Test :-).
+   */
+  public void testApp() {
+  }
 }
