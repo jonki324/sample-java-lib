731efdcb5a5676993a4ee1c9a812b9d35085ad98
jonki324 <jonki324@gmail.com>
2021-01-15 02:44:23 +0900

    JDBC操作追加

---
+++ src/main/java/com/example/db/JdbcMain.java
@@ -0,0 +1,14 @@
+package com.example.db;
+
+import com.example.common.Command;
+
+/**
+ * JDBCでDB操作実行クラス.
+ */
+public class JdbcMain implements Command {
+  @Override
+  public void execute() {
+    UserService service = new UserService();
+    service.createTable();
+  }
+}
