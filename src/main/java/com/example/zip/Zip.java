cd576cb2cb898b3dd48fae8869b50205f8958f2b
jonki324 <jonki324@gmail.com>
2021-01-26 01:47:52 +0900

    Zip解凍追加

--- src/main/java/com/example/zip/Zip.java
+++ src/main/java/com/example/zip/Zip.java
@@ -1,7 +1,9 @@
 package com.example.zip;
 
 import com.example.common.Command;
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -9,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,6 +23,7 @@ public class Zip implements Command {
   @Override
   public void execute() {
     create();
+    read();
   }
 
   private void create() {
@@ -32,6 +36,28 @@ public class Zip implements Command {
       ZipEntry zip = new ZipEntry("user.csv");
       zos.putNextEntry(zip);
       zos.write(data);
+    } catch (IOException e) {
+      log.error("IOエラー", e);
+    }
+  }
+
+  private void read() {
+    String input = "out.zip";
+    String output = "./out/";
+    try (FileInputStream fis = new FileInputStream(input);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        ZipInputStream zis = new ZipInputStream(bis, Charset.forName("UTF-8"))) {
+      ZipEntry zipEntry;
+      while ((zipEntry = zis.getNextEntry()) != null) {
+        try (FileOutputStream fos = new FileOutputStream(output + zipEntry.getName());
+            BufferedOutputStream bos = new BufferedOutputStream(fos);) {
+          byte[] data = new byte[1024];
+          int count = 0;
+          while ((count = zis.read(data)) != -1) {
+            bos.write(data, 0, count);
+          }
+        }
+      }
     } catch (IOException e) {
       log.error("IOエラー", e);
     }
